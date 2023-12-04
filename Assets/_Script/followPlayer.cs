da11570dc8f2a060ada6d507cfc4faf04e58e5e9
Melozing <nguyentrongquymanh@gmail.com>
2023-12-04 15:08:59 +0700

    Update (12/04/2023)

--- Assets/_Script/followPlayer.cs
+++ Assets/_Script/followPlayer.cs
@@ -5,10 +5,13 @@ public class followPlayer : MonoBehaviour
     public Transform player;
     public float speed = 27f;
     public float disLimit = 6f;
+    public float randPos = 0;
 
     private void Start()
     {
         this.player = PlayerCtrl.instance.transform;
+
+        this.randPos = Random.Range(-6, 6);
     }
 
     // Update is called once per frame
@@ -19,10 +22,14 @@ public class followPlayer : MonoBehaviour
 
     private void Follow()
     {
-        Vector3 distance = this.player.position - transform.position;
+        Vector3 pos = this.player.position;
+
+        pos.x = randPos;
+
+        Vector3 distance = pos - transform.position;
         if (distance.magnitude >= this.disLimit)
         {
-            Vector3 targetPoint = this.player.position - distance.normalized * this.disLimit;
+            Vector3 targetPoint = pos - distance.normalized * this.disLimit;
             transform.position = Vector3.MoveTowards(transform.position, targetPoint, this.speed * Time.fixedDeltaTime);
         }
     }
